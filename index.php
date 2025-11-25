ad221e774a0cf86b054f4dc25939133c1872e069
Luisxrl <infantemoreladamluis@gmail.com>
2025-11-25 14:35:41 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Inicio </title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+
+<header class="header">
+    <h1>Bienvenido</h1>
+    <center>
+    <nav>
+        <a href="index.php">Inicio</a>
+        <a href="registro.php">Registro</a>
+        <a href="login.php">Inicio de sesión</a>
+    </nav>
+    </center>
+</header>
+
+<h2> Proyecto Punto Cero</h2>
+
+
+<div class="cards">
+
+    <div class="card">
+        <img src="imagenNA.jpg" alt="">
+        <h3>Nathalia Corniel</h3>
+        <p> #4</p>
+        <p>Jefa</p>
+        <p>Creo lo que es la base de datos y la estructura del proyecto</p>
+    </div>
+
+    <div class="card">
+        <img src="imagenisa.jpeg" alt="">
+        <h3>Isairis Ferrera</h3>
+        <p>#7</p>
+        <p>Secretaria</p>
+        <p>Creo lo que es card y el inicio de seccion</p>
+
+    </div>
+
+    <div class="card">
+        <img src="imagenadam.jpg" alt="">
+        <h3>Adam </h3>
+        <p>#9</p>
+        <p>Supervisor</p>
+        <p>Creo lo que es el menu y subio a github</p>
+
+    </div>
+
+     <div class="card">
+        <img src="imagenery.jpg" alt="">
+        <h3>Ery Corona </h3>
+        <p>#5</p>
+        <p>Empleado</p>
+        <p>C</p>
+        <P>Creo lo que es la conexion y parte el login</P>
+    </div>
+
+</div>
+
+<footer>
+    <p>Adios proyecto finaL</p>
+</footer>
+
+</body>
+</html>
